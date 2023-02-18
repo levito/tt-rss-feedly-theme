ae5702e1e8eaebc9ac7dc95b387810c7a48040d4
Veit Lehmann <levito82@gmail.com>
2023-02-18 21:54:08 +0100

    feat(local-overrides): make login mobile-friendly

---
+++ local-overrides.js
@@ -0,0 +1,6 @@
+(function () {
+  var meta = document.createElement("meta");
+  meta.name = "viewport";
+  meta.content = "width=device-width, initial-scale=1.0";
+  document.querySelector("head").appendChild(meta);
+})();
