dc838f4f387f4d9c2a9037308938a8e18a0bc207
Veit Lehmann <levito82@gmail.com>
2023-02-21 22:21:01 +0100

    feat(local-overrides): add polyfill to support Safari
    This adds a simple polyfill for `requestIdleCallback` so tt-rss will work in Safari without modifying tt-rss or Safari experimental flags.

--- local-overrides.js
+++ local-overrides.js
@@ -1,6 +1,28 @@
 (function () {
+  // make login and other ttrss_utility pages mobile-friendly (needs local-overrides.css)
   var meta = document.createElement("meta");
   meta.name = "viewport";
   meta.content = "width=device-width, initial-scale=1.0";
   document.querySelector("head").appendChild(meta);
+
+  // polyfill for Safari, from https://github.com/pladaria/requestidlecallback-polyfill/blob/master/index.js
+  window.requestIdleCallback =
+    window.requestIdleCallback ||
+    function (cb) {
+      var start = Date.now();
+      return setTimeout(function () {
+        cb({
+          didTimeout: false,
+          timeRemaining: function () {
+            return Math.max(0, 50 - (Date.now() - start));
+          },
+        });
+      }, 1);
+    };
+
+  window.cancelIdleCallback =
+    window.cancelIdleCallback ||
+    function (id) {
+      clearTimeout(id);
+    };
 })();
